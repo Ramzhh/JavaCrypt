08837476e48bab0910e1b18078fb29a6d3e3e9c9
eskopp <liffecs@gmail.com>
2023-07-16 03:03:49 +0200

    make Java Runnable (chmod)

--- makefile.sh
+++ makefile.sh
@@ -1,3 +1,3 @@
 javac -Xlint:unchecked ./javacrypt/MyCryptMain.java
-jar cfm MyCrypt.jar ./javacrypt/makefile.txt javacrypt/*.class
-
+jar cfm JavaCrypt.jar ./javacrypt/makefile.txt javacrypt/*.class
+chmod +x JavaCrypt.jar
